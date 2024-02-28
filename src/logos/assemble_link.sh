b3fe16c21e09c322292338f40babf255c57a56c9
Jan Meznik <jan@meznik.dk>
2024-02-28 22:40:33 +0100

    Update assemble link script

--- src/logos/assemble_link.sh
+++ src/logos/assemble_link.sh
@@ -2,7 +2,7 @@
 
 echo "This script is just for testing purpose"
 
-aarch64-linux-gnu-as -o program.o program.as
-aarch64-linux-gnu-ld -o program program.o
+as -o program.o program.as
+ld -o program program.o
 
 echo "Done" 
